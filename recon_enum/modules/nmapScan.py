116b8bc58236f448de559791791dafa54e91c362
root <root@localhost.localdomain>
2018-11-26 20:21:50 -1000

    changed nmap to single process for now

--- recon_enum/modules/nmapScan.py
+++ recon_enum/modules/nmapScan.py
@@ -1,24 +1,10 @@
 import subprocess
 from modules.utility_functions import *
 import multiprocessing
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Manager
 import re
-import modules.udpScan as udpScan
-import modules.dig as dig
-import modules.dirb as dirb
-import modules.ftpEnum as ftpEnum
-import modules.httpEnum as httpEnum
-import modules.httpsEnum as httpsEnum
-import modules.mssqlEnum as mssqlEnum
-import modules.nikto as nikto
-import modules.pop3scan as pop3scan
-import modules.smbEnum as smbEnum
-import modules.smbNmap as smbNmap
-import modules.smtpEnum as smtpEnum
-import modules.sshScan as sshScan
-import modules.udpScan as udpScan
 
-def nmapScan(ip_address,scan_type,udp=False):
+def nmapScan(ip_address,scan_type,return_dict):
     ip_address = ip_address.strip()
     print(bcolors.OKGREEN + "INFO: Running general TCP/UDP nmap scans for " + ip_address + bcolors.ENDC)
     # STEALTH FIN SCAN
@@ -35,72 +21,5 @@ def nmapScan(ip_address,scan_type,udp=False):
     print(bcolors.OKGREEN + "INFO: RESULT BELOW - Finished with BASIC Nmap-scan for " + ip_address + bcolors.ENDC)
     print(results.decode())
     write_to_file(ip_address, "portscan", results)
-
-    # UDP SCAN GOES HERE BUT LETS COMMENT IT OUT FOR NOW AND MOVE IT BELOW LATER
-    if udp==False:
-        
-        udpScan.udpScan(ip_address)
-    # udpScan(ip_address)
-
-    lines = results.split(b"\n")
-    serv_dict = {}
-    for line in lines:
-        ports = []
-        line = str(line.decode('UTF-8').strip())
-        if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
-            # print line
-            while "  " in line:
-                line = line.replace("  ", " ")
-            linesplit= line.split(" ")
-            service = linesplit[2] # grab the service name
-
-            port = line.split(" ")[0] # grab the port/proto
-            # print port
-            if service in serv_dict:
-                ports = serv_dict[service] # if the service is already in the dict, grab the port list
-
-            ports.append(port)
-            # print ports
-            serv_dict[service] = ports # add service to the dictionary along with the associated port(2)
-    # stop here for now if stealth or passive
-    if scan_type=='STEALTH' or 'PASSIVE':
-        return
-   # go through the service dictionary to call additional targeted enumeration functions
-    for serv in serv_dict:
-        ports = serv_dict[serv]
-        if re.search(r"http[^s]", serv):
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(httpEnum.httpEnum, ip_address, port)
-        elif re.search(r"https|ssl", serv):
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(httpsEnum.httpsEnum, ip_address, port)
-        elif "smtp" in serv:
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(smtpEnum.smtpEnum, ip_address, port)
-        elif "ftp" in serv:
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(ftpEnum.ftpEnum, ip_address, port)
-        elif ("microsoft-ds" in serv) or ("netbios-ssn" == serv):
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(smbEnum.smbEnum, ip_address, port)
-                multProc(smbNmap.smbNmap, ip_address, port)
-        elif "ms-sql" in serv:
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(mssqlEnum.mssqlEnum, ip_address, port)
-        elif "ssh" in serv:
-            for port in ports:
-                port = port.split("/")[0]
-                multProc(sshScan.sshScan, ip_address, port)
-        # this snmp doesnt even exist
-        elif "snmp" in serv:
-            for port in ports:
-               port = port.split("/")[0]
-               multProc(snmpEnum.snmpEnum, ip_address, port)
-
-    return
+    # set ip = results in dict
+    return_dict[ip_address] = results
--- recon_enum/reconscan.py
+++ recon_enum/reconscan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import time
 import multiprocessing
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Manager
 import subprocess
 import sys
 import argparse
@@ -37,31 +37,85 @@ if __name__=='__main__':
     configure_scan(parser)
  
     args= parser.parse_args()
-
+    # setup a dictionary for nmap return values to scan
+    #manager = multiprocessing.Manager()
+    #return_dict = manager.dict()
+    return_dict={}
     # now we perform scans
     for ip in args.ip:
         # do a dig first regardless of mode
         p = multiprocessing.Process(target=dig.dig, args=(ip,))
         p.start()
         # now do the appropriate nmap for mode type
-        p = multiprocessing.Process(target=nmapScan.nmapScan, args=(ip,args.mode,args.udp,))
-        p.start()
-        # if args.mode=='STEALTH':
-        #     p = multiprocessing.Process(target=nmapScan.nmapScan, args=(ip,args.mode,args.udp,))
-        #     p.start()
-        #     break
-        # if args.mode=='PASSIVE':
-        #     nmapScan(ip,args.mode,args.udp)
-        #     break
-        # if args.mode=='ACTIVE':
-        #     nmapScan(ip,args.mode,args.udp)
-        #     break
-        # if args.mode=='ALL':
-        #     #nmapScan(ip,args.mode,args.udp)
-        #     p = multiprocessing.Process(target=nmapScan.nmapScan, args=(ip,args.mode,args.udp,))
-        #     p.start()
-        #     break
-    #     # next use nmap
-    #     p = multiprocessing.Process(target=nmapScan, args=(scanip,))
-    #     p.start()
+        nmapScan.nmapScan(ip,args.mode,return_dict,)
+        #p = multiprocessing.Process(target=nmapScan.nmapScan, 
+        #                            args=(ip,args.mode,return_dict,))
+        #p.start()
+        # UDP SCAN GOES HERE BUT LETS COMMENT IT OUT FOR NOW AND MOVE IT BELOW LATER
+        if args.udp==False: 
+            udpScan.udpScan(ip_address)
+        
+        results = return_dict[ip]
+        lines = results.split(b"\n")
+        serv_dict = {}
+        for line in lines:
+            ports = []
+            line = str(line.decode('UTF-8').strip())
+            if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
+                # print line
+                while "  " in line:
+                    line = line.replace("  ", " ")
+                linesplit= line.split(" ")
+                service = linesplit[2] # grab the service name
+
+                port = line.split(" ")[0] # grab the port/proto
+                # print port
+                if service in serv_dict:
+                    ports = serv_dict[service] # if the service is already in the dict, grab the port list
+
+                ports.append(port)
+                # print ports
+                serv_dict[service] = ports # add service to the dictionary along with the associated port(2)
+        # stop here for now if stealth or passive
+        if args.mode=='STEALTH' or args.mode=='PASSIVE':
+            print("Scan complete")
+            sys.exit()
+    # go through the service dictionary to call additional targeted enumeration functions
+        for serv in serv_dict:
+            ports = serv_dict[serv]
+            if re.search(r"http[^s]", serv):
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(httpEnum.httpEnum, ip_address, port)
+            elif re.search(r"https|ssl", serv):
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(httpsEnum.httpsEnum, ip_address, port)
+            elif "smtp" in serv:
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(smtpEnum.smtpEnum, ip_address, port)
+            elif "ftp" in serv:
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(ftpEnum.ftpEnum, ip_address, port)
+            elif ("microsoft-ds" in serv) or ("netbios-ssn" == serv):
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(smbEnum.smbEnum, ip_address, port)
+                    multProc(smbNmap.smbNmap, ip_address, port)
+            elif "ms-sql" in serv:
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(mssqlEnum.mssqlEnum, ip_address, port)
+            elif "ssh" in serv:
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(sshScan.sshScan, ip_address, port)
+            # this snmp doesnt even exist
+            elif "snmp" in serv:
+                for port in ports:
+                    port = port.split("/")[0]
+                    multProc(snmpEnum.snmpEnum, ip_address, port)
+
 
